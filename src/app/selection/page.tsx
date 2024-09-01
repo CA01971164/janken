b71bad42a235f95ffd68a502e0e9076fa79d1d1d
01971164 <CA01971164@st.kawahara.ac.jp>
2024-09-01 15:30:55 +0900

    add query

--- src/app/selection/page.tsx
+++ src/app/selection/page.tsx
@@ -8,13 +8,13 @@ function selection() {
       <p>
         下の手の形から、好きなものを選んでね。選ぶと、選んだ手で勝負できるよ。
       </p>
-      <Link href="/computer">
+      <Link href={{ pathname: "/computer", query: { choice: "rock" } }}>
         <Image src="/rock.png" alt="rock" width={50} height={50} />
       </Link>
-      <Link href="/computer">
+      <Link href={{ pathname: "/computer", query: { choice: "scissors" } }}>
         <Image src="/scissors.png" alt="scissors" width={50} height={50} />
       </Link>
-      <Link href="/computer">
+      <Link href={{ pathname: "/computer", query: { choice: "paper" } }}>
         <Image src="/paper.png" alt="paper" width={50} height={50} />
       </Link>
     </>
