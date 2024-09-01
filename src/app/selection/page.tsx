2a4614537bb9715f355fe89fdd0e9261dc3426dd
01971164 <CA01971164@st.kawahara.ac.jp>
2024-09-01 13:09:15 +0900

    tag to component

--- src/app/selection/page.tsx
+++ src/app/selection/page.tsx
@@ -1,18 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function selection() {
-    return (
-      <>
-          <h1>じゃんけん</h1>
-          <p>下の手の形から、好きなものを選んでね。選ぶと、選んだ手で勝負できるよ。</p>
-          <ul>
-            <li className="result"><img src="../images/rock.png" alt="グー" /></li>
-            <li className="result">
-              <img src="../images/scissors.png" alt="チョキ" />
-            </li>
-            <li className="result"><img src="../images/paper.png" alt="パー" /></li>
-          </ul>
-      </>
-    );
-  }
-  
-  export default selection;
-  
+  return (
+    <>
+      <h1>じゃんけん</h1>
+      <p>
+        下の手の形から、好きなものを選んでね。選ぶと、選んだ手で勝負できるよ。
+      </p>
+      <Link href="/computer">
+        <Image src="/rock.png" alt="rock" width={50} height={50} />
+      </Link>
+      <Link href="/computer">
+        <Image src="/scissors.png" alt="scissors" width={50} height={50} />
+      </Link>
+      <Link href="/computer">
+        <Image src="/paper.png" alt="paper" width={50} height={50} />
+      </Link>
+    </>
+  );
+}
+
+export default selection;
