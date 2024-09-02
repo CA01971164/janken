800d9089efafca66b7022cd29f5cef999a00c0a6
01971164 <CA01971164@st.kawahara.ac.jp>
2024-09-02 16:53:21 +0900

    add  button to move with keyboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
 import Link from "next/link";
 import "./globals.css";
 
 export default function Home() {
+  const handleKeydown = document.addEventListener("keydown", (event: any) => {
+    window.location.href = "/explanation";
+  });
   return (
     <>
       <h1 className="text-5xl">★ジャンケンゲーム★</h1>
 
-      <Link href="/explanation">
-        <button className="btn ">start</button>
+      <Link href="/explanation" onKeyDown={handleKeydown}>
+        <button className="btn">start</button>
       </Link>
     </>
   );
